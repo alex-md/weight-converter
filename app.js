6c195913598674e34080eaa779aa070477d35703
Alex Johnson <alexrnau@gmail.com>
2021-01-13 23:09:26 -0700

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ document.getElementById('lbsInput').addEventListener('input', ({ target }) => {
 * @param {number} lbs
 */
 function OutputConversion(lbs) {
-    document.getElementById('gramsOutput').innerHTML = ("~ " + lbs * 454).toLocaleString(); //converts to grams, adds commas
-    document.getElementById('kilogramsOutput').innerHTML = ("~ " + lbs / 2.205).toLocaleString(); //converts to kilograms, adds commas
+    document.getElementById('gramsOutput').innerHTML = ("~ " + (lbs * 454).toLocaleString()); //converts to grams, adds commas
+    document.getElementById('kilogramsOutput').innerHTML = ("~ " + (lbs / 2.205).toLocaleString()); //converts to kilograms, adds commas
     document.getElementById('ouncesOutput').innerHTML = (lbs * 16).toLocaleString(); //converts to ounces, adds commas
 }
