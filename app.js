ed7e6e6075a1175d905426488d2013135704d5d0
Alex Johnson <alexrnau@gmail.com>
2021-01-13 22:46:43 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-document.getElementById('lbsInput').addEventListener('input', function (output) {
-  var lbs = output.target.value; //variable lbs = function(ouput) pass with .target.value
-  OutputConversion(lbs); // running function
+"use strict";
+document.getElementById('lbsInput').addEventListener('input', ({ target }) => {
+    // @ts-ignore
+    const lbs = target.value; //variable lbs = function(ouput) pass with .target.value
+    OutputConversion(lbs); // running function
 });
-
+/**
+* @param {number} lbs
+*/
 function OutputConversion(lbs) {
-  document.getElementById('gramsOutput').innerHTML = (lbs * 454).toLocaleString(); //converts to grams, adds commas
-  document.getElementById('kilogramsOutput').innerHTML = (lbs / 2.205).toLocaleString();//converts to kilograms, adds commas
-  document.getElementById('ouncesOutput').innerHTML = (lbs * 16).toLocaleString();//converts to ounces, adds commas
+    document.getElementById('gramsOutput').innerHTML = (lbs * 454).toLocaleString(); //converts to grams, adds commas
+    document.getElementById('kilogramsOutput').innerHTML = (lbs / 2.205).toLocaleString(); //converts to kilograms, adds commas
+    document.getElementById('ouncesOutput').innerHTML = (lbs * 16).toLocaleString(); //converts to ounces, adds commas
 }
